985c40bf761c1dfda52abdcddff777fd5926c478
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-03-25 23:42:34 +0500

    Relationship in Java

---
+++ dbExample/dbExample/src/main/java/com/example/dbExample/DbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbExampleApplication.class, args);
+	}
+
+}
---
+++ dbExample/dbExample/src/test/java/com/example/dbExample/DbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
