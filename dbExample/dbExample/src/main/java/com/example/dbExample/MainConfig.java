36cc958d0206bf4c945a042d055f8124961ef91a
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-03-26 23:07:10 +0500

    Adding Add user URL

---
+++ dbExample/dbExample/src/main/java/com/example/dbExample/MainConfig.java
@@ -0,0 +1,23 @@
+package com.example.dbExample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class MainConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(AbstractHttpConfigurer::disable);
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/api/addUser").permitAll());
+
+        return httpSecurity.build();
+    }
+}
