f08080ab7571a695decbe57fbb2a5da49eb6a3c6
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-01 23:46:47 +0500

    Project Structure

---
+++ dbExample/dbExample/src/main/java/com/example/dbExample/model/User.java
@@ -0,0 +1,31 @@
+package com.example.dbExample.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column(name = "Email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "Password", nullable = false)
+    private String password;
+}
