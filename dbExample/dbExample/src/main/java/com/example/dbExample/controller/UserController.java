c521536a3049704191b214d31eb679767a3a17e7
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-03 16:33:10 +0500

    user details

--- dbExample/dbExample/src/main/java/com/example/dbExample/controller/UserController.java
+++ dbExample/dbExample/src/main/java/com/example/dbExample/controller/UserController.java
@@ -1,17 +1,34 @@
 package com.example.dbExample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dbExample.model.User;
 import com.example.dbExample.payload.UserDTO;
+import com.example.dbExample.services.UserService;
 
 import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
 
-    public ResponseEntity<?> addUser(@Valid @RequestBody UserDTO userDTO) {
+    @Autowired
+    private UserService userService;
 
+    public ResponseEntity<?> addUser(@Valid @RequestBody UserDTO userDTO) {
+        try {
+            User user = new User();
+            user.setEmail(userDTO.getEmail());
+            user.setPassword(userDTO.getPassword());
+            System.out.println("USER INFORMATION : " + user + userDTO);
+            userService.save(user);
+            return ResponseEntity.ok(200);
+        } catch (Exception e) {
+            log.debug(AccountStatus.ADD_ACCOUNT_ERROR.toString() + ": " + e.getMessage());
+            String errorMessage;
+            HttpStatus status = HttpStatus.BAD_REQUEST;
+        }
     }
 }
--- dbExample/dbExample/src/main/java/com/example/dbExample/services/UserService.java
+++ dbExample/dbExample/src/main/java/com/example/dbExample/services/UserService.java
@@ -1,7 +1,11 @@
 package com.example.dbExample.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.dbExample.Repository.UserRepo;
@@ -12,8 +16,17 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
-    @SuppressWarnings("null")
     public User save(User user) {
         return userRepo.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepo.findByEmail((email));
+        if (!optionalUser.isPresent()) {
+            throw new UsernameNotFoundException("User Not Found");
+        }
+        User user = optionalUser.get();
+        return new User(user.getEmail(), user.getPassword());
     }
 }
